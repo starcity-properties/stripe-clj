73cdd0ccaeffca2794d893ddf0e323a0e0533a83
Josh Lehman <jalehman37@gmail.com>
2017-12-17 18:43:38 -0800

    Version 0.3.6-SNAPSHOT, bump deps

--- project.clj
+++ project.clj
@@ -1,18 +1,12 @@
-(defproject racehub/stripe-clj "0.3.6-SNAPSHOT"
-  :description "Schemafied Stripe bindings for Clojure."
-  :url "https://github.com/racehub/stripe-clj"
+(defproject starcity/stripe-clj "0.3.6-SNAPSHOT"
+  :description "Stripe bindings for Clojure."
+  :url "https://github.com/starcity/stripe-clj"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[cheshire "5.5.0"]
-                 [environ "0.5.0"]
-                 [http-kit "2.1.19"]
-                 [org.clojure/clojure "1.7.0"]
-                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-                 [racehub/schema "0.4.4"]]
-  :jar-exclusions [#"\.cljx|\.swp|\.swo|\.DS_Store"]
-  :source-paths ["src/clj"]
-  :resource-paths ["src/cljs" "resources"]
-  :profiles {:dev {:injections [(require 'schema.core)
-                                (schema.core/set-fn-validation! true)]
-                   :test-paths ["test/clj"]
-                   :plugins [[paddleguru/lein-gitflow "0.1.2"]]}})
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/core.async "0.3.465"]
+                 [http-kit "2.2.0"]
+                 [cheshire "5.8.0"]
+                 [starcity/toolbelt-async "0.3.0"]
+                 [starcity/toolbelt-core "0.2.0"]]
+  :source-paths ["src/clj"])
