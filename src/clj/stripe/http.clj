1d308493fb242f3f1f3d6ba97d520e48068de6b1
Josh Lehman <jalehman37@gmail.com>
2017-12-17 18:51:57 -0800

    rewrite to use spec, add connected accounts

--- src/clj/stripe/http.clj
+++ src/clj/stripe/http.clj
@@ -1,49 +1,77 @@
 (ns stripe.http
   (:require [cheshire.core :as json]
             [clojure.core.async :as a]
-            [environ.core :as e]
+            [clojure.spec.alpha :as s]
             [org.httpkit.client :as http]
-            [schema.core :as s]
             [stripe.schema :as ss]
-            [stripe.util :as u]))
+            [stripe.util :as u]
+            [stripe.util.codec :as codec]
+            [toolbelt.async :as ta]
+            [toolbelt.core :as tb]))
 
-;; ## Schema
+;; =============================================================================
+;; Spec
+;; =============================================================================
 
-(def ^:dynamic url "https://api.stripe.com/v1/")
 
-(s/defschema ApiToken
-  (s/named s/Str "Secret stripe key."))
+(s/def ::api-token string?)
 
-(s/defschema Expansion
-  "Supply a keyword like :balance_transaction or a sequence to expand
-  multiple fields. To expand nested fields, string the items together
-  in the keyword with a dot, like :balance_transaction.source."
-  (s/either s/Keyword [s/Keyword]))
+(s/def ::expansion
+  (s/or :keyword keyword? :keyseq (s/+ keyword?)))
 
-(s/defschema HttpKitOptions
-  "Options for Http-Kit's client."
-  {s/Any s/Any})
+(s/def ::http-kit-options map?)
 
-(s/defschema StripeParams
-  "Post data or Get req data for Stripe."
-  {s/Any s/Any})
+(s/def ::stripe-params map?)
 
-;; ## Authorization
+(s/def ::method #{http/post http/get http/delete})
+
+(s/def ::out-ch ta/chan?)
+
+(s/def ::client-options ::http-kit-options)
+
+(s/def ::token ::api-token)
+
+(s/def ::account string?)
+
+(s/def ::request-options
+  (s/keys :opt-un [::account ::out-ch ::stripe-params ::client-options ::token]))
+
+(s/def ::endpoint string?)
+
+(s/def ::api-call
+  (s/keys :req-un [::method ::endpoint]
+          :opt-un [::account ::out-ch ::stripe-params ::client-options ::token]))
+
+
+;; =============================================================================
+;; Authorization
+;; =============================================================================
+
 
 (def ^:dynamic *token* nil)
 (def ^:dynamic *api-version* nil)
+(def ^:dynamic *connect-account* nil)
 
-(s/defn api-token :- (s/maybe ApiToken)
-  []
-  (or *token* (:stripe-secret e/env)))
 
-(s/defn api-version :- (s/maybe s/Str)
-  []
-  (or *api-version* (:stripe-api-version e/env)))
+;; =====================================
+;; API Token
 
-(defmacro with-token [k & forms]
-  `(binding [*token* ~k]
+
+(defn api-token [] *token*)
+
+(s/fdef api-token
+        :ret (ss/maybe ::api-token))
+
+
+(defmacro with-token [t & forms]
+  `(binding [*token* ~t]
      ~@forms))
+
+(s/fdef with-token
+        :args (s/cat :token ::api-token
+                     :forms (s/* list?))
+        :ret list?)
+
 
 (defn use-token!
   "Permanently sets a base token. The token can still be overridden on
@@ -51,10 +79,27 @@
   [s]
   (alter-var-root #'*token* (constantly s)))
 
+
+;; =====================================
+;; API Version
+
+
+(defn api-version [] *api-version*)
+
+(s/fdef api-version
+        :ret (ss/maybe string?))
+
+
 (defmacro with-api-version
   [v & forms]
   `(binding [*api-version* ~v]
      ~@forms))
+
+(s/fdef with-api-version
+        :args (s/cat :version string?
+                     :forms (s/* list?))
+        :ret list?)
+
 
 (defn use-api-version!
   "Permanently sets an API version. The api version can still be
@@ -63,78 +108,118 @@
   (alter-var-root #'*api-version* (constantly s)))
 
 
-;; ## Private
+;; =====================================
+;; Connect Account
 
-(s/defn method-url :- s/Str
+
+(defn connect-account [] *connect-account*)
+
+(s/fdef connect-account
+        :ret (ss/maybe string?))
+
+
+(defmacro with-connect-account
+  [v & forms]
+  `(binding [*connect-account* ~v]
+     ~@forms))
+
+(s/fdef with-connect-account
+        :args (s/cat :version string?
+                     :forms (s/* list?))
+        :ret list?)
+
+
+(defn use-connect-account!
+  "Permanently sets an API version. The api version can still be
+  overridden on a per-thread basis using with-connect-account."
+  [s]
+  (alter-var-root #'*connect-account* (constantly s)))
+
+
+;; =============================================================================
+;; Private
+;; =============================================================================
+
+
+(def ^:dynamic url "https://api.stripe.com/v1/")
+
+
+(defn method-url
   "URL for calling a method."
-  [method :- s/Str]
+  [method]
   (str url method))
 
-(s/defn prepare-expansion :- [s/Str]
-  "Turns the expansion's keywords into strings. In the future, this
-  might validate that all expansions are reliable jumps based on the
-  Stripe API's allowed expansion fields for each object."
-  [expand :- Expansion]
+(s/fdef method-url
+        :args (s/cat :method string?)
+        :ret string?)
+
+
+(defn prepare-expansion
+  [expand]
   (map name (u/collectify expand)))
 
-(s/defn prepare-params :- {s/Any s/Any}
+(s/fdef prepare-expansion
+        :args (s/cat :expand ::expansion)
+        :ret (s/+ string?))
+
+
+(defn- encode-params
+  [method params]
+  (case method
+    :get [:query-params params]
+    [:body (codec/form-encode params)]))
+
+
+(defn prepare-params
   "Returns a parameter map suitable for feeding in to a request to Stripe.
 
   `opts` is a set of options for http-kit's client. These kick out the
   defaults.
 
   `params` is the parameters for the stripe API calls."
-  [token :- ApiToken
-   params :- StripeParams
-   opts :- HttpKitOptions]
-  (let [params (if-let [expand (:expand params)]
-                 (-> (dissoc params :expand)
-                     (assoc "expand[]" (prepare-expansion expand)))
-                 params)
-        base-params {:basic-auth token
-                     :query-params params
-                     :throw-exceptions false}
-        version (when-let [v (or (:api-version opts) (api-version))]
-                  {:headers (merge {"Stripe-Version" v}
-                                   (:headers opts))})]
-    (merge base-params version (dissoc opts :api-version))))
+  [token method params opts]
+  (let [params      (if-let [expand (:expand params)]
+                      (-> (dissoc params :expand)
+                          (assoc "expand[]" (prepare-expansion expand)))
+                      params)
+        [k params'] (encode-params method params)
+        base-params {:basic-auth       token
+                     :throw-exceptions false
+                     k                 params'}
+        version     (or (:api-version opts) (api-version))
+        connect     (or (:account opts) (connect-account))
+        headers     (tb/assoc-when {} "Stripe-Version" version "Stripe-Account" connect)]
+    (merge base-params {:headers headers} (dissoc opts :api-version :account))))
 
-;; ## Public Methods
-;;
-;; These are a mishmash of the stripe API methods, and should probably
-;; be broken out into a bunch of sub namespaces.
-;;
-;; ## Method Call Helpers
+(s/fdef prepare-params
+        :args (s/cat :token ::api-token
+                     :method ::method
+                     :params ::stripe-params
+                     :opts ::http-kit-options)
+        :ret map?)
 
-(def Method
-  (s/enum http/post http/get http/delete))
+;; =============================================================================
+;; Public API
+;; =============================================================================
 
-(s/defschema RequestOptions
-  {(s/optional-key :out-ch) (ss/Channel)
-   (s/optional-key :stripe-params) StripeParams
-   (s/optional-key :client-options) HttpKitOptions
-   (s/optional-key :token) ApiToken})
 
-(s/defschema ApiCall
-  (assoc RequestOptions
-    :method Method
-    :endpoint s/Str))
-
-(s/defn api-call :- (ss/Async)
+(defn api-call
   "Call an API method on Stripe. If an output channel is supplied, the
   method will place the result in that channel; if not, returns
   synchronously."
-  [{:keys [stripe-params client-options token method endpoint out-ch]
-    :or {stripe-params {}
-         client-options {}
-         token (api-token)}} :- ApiCall]
+  [{:keys [stripe-params client-options token account method endpoint out-ch]
+    :or   {stripe-params  {}
+           client-options {}
+           account        *connect-account*
+           token          (api-token)}}]
   (assert token "API Token must not be nil.")
-  (let [url (method-url endpoint)
-        params (prepare-params token stripe-params client-options)
+  (let [url     (method-url endpoint)
+        params  (->> (assoc client-options :account account)
+                     (prepare-params token method stripe-params))
         process (fn [ret]
                   (or (json/parse-string (:body ret) keyword)
                       {:error (:error ret)}))]
-    (if-not out-ch
+    (if-not (some? out-ch)
       (process @(method url params))
       (do (method url params
                   (fn [ret]
@@ -142,17 +227,26 @@
                     (a/close! out-ch)))
           out-ch))))
 
+(s/fdef api-call
+        :args (s/cat :params ::api-call)
+        :ret (s/or :result map? :chan ta/chan?))
+
+
 (defmacro defapi
   "Generates a synchronous and async version of the same function."
   [sym method]
-  `(s/defn ~sym
-     ([endpoint# :- s/Str]
-        (~sym endpoint# {}))
-     ([endpoint# :- s/Str opts# :- RequestOptions]
-        (api-call
-         (assoc opts#
-           :method ~method
-           :endpoint endpoint#)))))
+  `(defn ~sym
+     ([endpoint#]
+      (~sym endpoint# {}))
+     ([endpoint# opts#]
+      (api-call
+       (assoc opts#
+              :method ~method
+              :endpoint endpoint#)))))
+
+(s/fdef defapi
+        :args (s/cat :symbol symbol? :method symbol?)
+        :ret list?)
 
 (defapi post-req http/post)
 (defapi get-req http/get)
