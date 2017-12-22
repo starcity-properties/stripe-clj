b3d3f1fc94008ddf45a007ff2fbc984f846ef6ec
Josh Lehman <jalehman37@gmail.com>
2017-12-22 13:03:54 -0800

    convert balance api to spec

--- src/clj/stripe/balance.clj
+++ src/clj/stripe/balance.clj
@@ -1,121 +1,202 @@
 (ns stripe.balance
   "Functions for Stripe's Balance API."
-  (:require [schema.core :as s]
+  (:require [clojure.spec.alpha :as s]
             [stripe.http :as h]
-            [stripe.schema :as ss]
-            [stripe.recipient :as r]))
+            [stripe.schema :as ss]))
 
-;; ## Balance API
+;; =============================================================================
+;; Spec
+;; =============================================================================
 
-;; ### Schema
+(s/def ::amount
+  pos-int?)
 
-(def BalanceAmount
-  {:amount s/Int
-   :currency ss/CurrencyID
-   (s/optional-key :source_types) {s/Keyword s/Int}})
+(s/def ::currency
+  :stripe.schema/currency-id)
 
-(def BalanceTxID
-  (s/named s/Str "ID of a balance transaction."))
+(s/def ::source_types
+  (s/map-of keyword? integer?))
 
-(def Balance
-  (-> {:livemode s/Bool
-       :available [BalanceAmount]
-       :pending [BalanceAmount]}
+(s/def ::balance-amount
+  (s/keys :req-un [::amount ::currency] :opt-un [::source_types]))
+
+(s/def ::balance-tx-id
+  string?)
+
+(s/def ::available
+  (s/+ ::balance-amount))
+
+(s/def ::pending
+  (s/+ ::balance-amount))
+
+(s/def ::balance
+  (-> (s/keys :req-un [::livemode
+                       ::available
+                       ::pending
+                       ::object])
       (ss/stripe-object "balance")))
 
-(def FeeDetails
-  "Details about this particular Stripe fee in the Balance Transaction
-  fee breakdown list."
-  {:amount s/Int
-   :currency ss/CurrencyID
-   :type s/Str
-   :description (s/maybe s/Str)
-   :application (s/maybe s/Str)})
+(s/def :fee-details/type
+  string?)
 
-(def TransactionType
-  (s/enum "charge" "refund" "adjustment" "application_fee"
-          "application_fee_refund" "transfer" "transfer_cancel"
-          "transfer_failure" "payment" "payment_refund"))
+(s/def ::description
+  (ss/maybe string?))
 
-(def BalanceTx
-  (-> {:id BalanceTxID
-       :amount s/Int
-       :available_on ss/UnixTimestamp
-       :created ss/UnixTimestamp
-       :currency ss/CurrencyID
-       :fee s/Int
-       :fee_details [FeeDetails]
-       :net s/Int
-       :status (s/enum "pending" "available")
-       :type TransactionType
-       :description (s/maybe s/Str)
-       :source (-> (s/either s/Str {s/Keyword s/Any})
-                   (s/named "The source Stripe object. Either the ID or,
-               if expanded, the object."))
-       (s/optional-key :recipient) (s/either (s/eq "self") r/RecipientID)}
-      (ss/stripe-object "balance_transaction")))
+(s/def ::application
+  (ss/maybe string?))
 
-;; ### Api Calls
+(s/def ::fee-details
+  (s/keys :req-un [::amount ::currency :fee-details/type ::description ::application]))
 
-(s/defn get-balance :- (ss/Async Balance)
+(s/def ::transaction-type
+  #{"charge" "refund" "adjustment" "application_fee"
+    "application_fee_refund" "transfer" "transfer_cancel"
+    "transfer_failure" "payment" "payment_refund"})
+
+(s/def ::id
+  ::balance-tx-id)
+
+(s/def ::available_on
+  ss/unix-timestamp?)
+
+(s/def ::created
+  ss/unix-timestamp?)
+
+(s/def ::fee
+  integer?)
+
+(s/def ::fee_details
+  (s/+ ::fee-details))
+
+(s/def ::net
+  integer?)
+
+(s/def ::status
+  #{"pending" "available"})
+
+(s/def :balance/type
+  ::transaction-type)
+
+(s/def ::source
+  (s/or :string string? :map map?))
+
+(s/def ::balance-tx
+  (-> (s/keys :req-un [::id ::amount ::available_on ::created ::currency ::fee
+                       ::fee_details ::net ::status :balance/type ::description
+                       ::source ::object])
+      (s/and (ss/stripe-object "balance_transaction"))))
+
+(s/def ::starting-after
+  string?)
+
+(s/def ::limit
+  pos-int?)
+
+(s/def ::history-options
+  (s/and (s/keys :opt-un [::starting-after ::limit])
+         :stripe.http/request-options))
+
+
+;; =============================================================================
+;; HTTP API
+;; =============================================================================
+
+
+(defn get-balance
   "Returns a channel containing the current account balance, based
-  on the API key that was used to make the request."
-  ([] (get-balance {}))
-  ([opts :- h/RequestOptions]
+   on the API key that was used to make the request."
+  ([]
+   (get-balance {}))
+  ([opts]
    (h/get-req "balance" opts)))
 
-(s/defn get-account-balance :- (ss/Async Balance)
-  [account-id :- s/Str]
-  (get-balance {:client-options {:headers {"Stripe-Account" account-id}}}))
+(s/fdef get-balance
+        :args (s/cat :opts (s/? :stripe.http/request-options))
+        :ret (ss/async ::balance))
 
-(s/defn get-account-history :- (ss/sublist [BalanceTx])
-  ([account-id :- s/Str]
-   (get-account-history account-id {}))
-  ([account-id :- s/Str
-    opts :- {(s/optional-key :starting-after) s/Str
-             (s/optional-key :limit) s/Str}]
+
+(defn get-history
+  ([]
+   (get-history {}))
+  ([opts]
    (h/get-req "balance/history"
-              {:stripe-params (merge {:limit (:limit opts 100)}
-                                     (when-let [sa (:starting-after opts)]
-                                       {:starting_after sa}))
-               :client-options {:headers {"Stripe-Account" account-id}}})))
+              (-> {:stripe-params (merge {:limit (:limit opts 100)}
+                                         (when-let [sa (:starting-after opts)]
+                                           {:starting_after sa}))}
+                  (merge opts)))))
 
-(s/defn get-all-account-history :- [BalanceTx]
-  [account-id :- s/Str]
-  (loop [page (get-account-history account-id)
-         history []]
-    (let [data (:data page)
-          all (concat history (:data page))]
-      (if (:has_more page)
-        (recur (get-account-history account-id {:starting-after (:id (last data))})
-               all)
-        all))))
+(s/fdef get-history
+        :args (s/cat :opts (s/? ::history-options))
+        :ret (ss/async (ss/sublist (s/* ::balance-tx))))
 
-(s/defn get-balance-tx :- (ss/Async BalanceTx)
+
+(defn get-all-history
+  "Synchronously fetches all transaction history for an optional Connect `account`; otherwise
+  will fetch from the platform account."
+  [& [account-id]]
+  (letfn [(fetch [& [after]]
+            (get-history (merge
+                          {}
+                          (when-some [a account-id] {:account account-id})
+                          (when-some [a after] {:starting-after a}))))]
+    (loop [page    (fetch)
+           history []]
+      (let [data (:data page)
+            all  (concat history (:data page))]
+        (if (:has_more page)
+          (recur (fetch (:id (last data))) all)
+          all)))))
+
+(s/fdef get-all-history
+        :args (s/cat :account (s/? string?))
+        :ret (s/* ::balance-tx))
+
+
+(defn get-balance-tx
   "Returns a channel that contains the balance transaction referenced
   by the supplied ID, or an error if it doesn't exist."
-  ([id :- BalanceTxID]
-     (get-balance-tx id {}))
-  ([id :- BalanceTxID opts :- h/RequestOptions]
-     (h/get-req (str "balance/history/" id) opts)))
+  ([id]
+   (get-balance-tx id {}))
+  ([id opts]
+   (h/get-req (str "balance/history/" id) opts)))
 
-;; ### Helpers
+(s/fdef get-balance-tx
+        :args (s/cat :transaction-id ::balance-tx-id
+                     :opts (s/? :stripe.http/request-options))
+        :ret (ss/async ::balance-tx))
 
-(s/defn available-amount :- ss/NonNegativeInt
+
+;; =============================================================================
+;; Selectors
+;; =============================================================================
+
+
+(defn available-amount
   "Returns the current amount in the given currency or USD currently
   available in our account."
-  [{:keys [available]} :- Balance
-   & opts]
-  (let [currency (:currency (first opts) "usd")]
+  [{:keys [available]} & [currency]]
+  (let [currency (or currency "usd")]
     (:amount (first (filter (comp #{currency} :currency) available)))))
 
-(s/defn pending-amount :- ss/NonNegativeInt
+(s/fdef available-amount
+        :args (s/cat :balance ::balance :currency (s/? ::currency))
+        :ret pos-int?)
+
+
+(defn pending-amount
   "Returns the current amount in the given currency or USD pending in our account."
-  [{:keys [pending]} :- Balance
-   & opts]
-  (let [currency (:currency (first opts) "usd")]
+  [{:keys [pending]} & [currency]]
+  (let [currency (or currency "usd")]
     (:amount (first (filter (comp #{currency} :currency) pending)))))
 
-(s/defn tx-fee :- s/Int
-  [tx :- BalanceTx]
+(s/fdef pending-amount
+        :args (s/cat :balance ::balance :currency (s/? ::currency))
+        :ret pos-int?)
+
+
+(defn tx-fee [tx]
   (:fee tx))
+
+(s/fdef tx-fee
+        :args (s/cat :transaction ::balance-tx)
+        :ret integer?)
