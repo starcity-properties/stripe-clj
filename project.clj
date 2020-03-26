698b0f065e1c24d448911488873fb8d539f02b37
Josh Lehman <josh@starcity.com>
2020-03-26 16:51:33 -0700

    Version 0.5.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject starcity/stripe-clj "0.5.0"
+(defproject starcity/stripe-clj "0.5.1-SNAPSHOT"
   :description "Stripe bindings for Clojure."
   :url "https://github.com/starcity-properties/stripe-clj"
   :license {:name "Eclipse Public License"
